b6753896c8ccfd447d3f621907d6ecfdd7ee3e8c
Hugh1996 <>
2024-02-10 17:44:07 +0000

    Bag bug fixed

--- checkout/views.py
+++ checkout/views.py
@@ -4,7 +4,7 @@ from .forms import OrderForm
 
 
 def checkout(request):
-    bag = request.session.getr('bag', {})
+    bag = request.session.get('bag', {})
     if not bag:
         messages.error(request, "There's nothing in your bag at the moment")
         return redirect(reverse('products'))
