bc874837678e0b304b5b2d954ab13973e00ff0ae
Hugh1996 <>
2024-02-13 22:45:52 +0000

    Bug checkout.html

--- checkout/views.py
+++ checkout/views.py
@@ -13,6 +13,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51Oic1sIQ0qr3vSUKF8dP1pHtBpg61hBQ0Qtr7oHWmuPNHI2CNwBR2PM09m6lpuPwgfwAWgW6m5ClOEA749RtDDFw00HtqXbds1',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
